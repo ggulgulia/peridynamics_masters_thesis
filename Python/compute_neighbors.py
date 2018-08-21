0005411ce70147fdef2abda2c9435218bb731793
gajendra <gajendra.gulgulia@tum.de>
2018-08-21 14:28:27 +0200

    Naive compute neighbor method implemented This method is O(N²) at the moment, look for optimization

--- Python/compute_neighbors.py
+++ Python/compute_neighbors.py
@@ -22,8 +22,8 @@ def peridym_set_horizon(mesh, horizon=None):
     lx_max = abs(corner_min[0] - corner_max[0])
     ly_max = abs(corner_min[1] - corner_max[1])
 
-    if (horizon > 0.3*min(lx_max, ly_max)):
-        refine_factor = math.ceil(horizon/(0.3*min(ly_max, lx_max)))
+    if (horizon > 0.2*min(lx_max, ly_max)):
+        refine_factor = math.ceil(horizon/(0.2*min(ly_max, lx_max)))
         print("cells are too coarse for peridynamic simulation\n",
                     "refining the cells with refine factor of %i\n"
                     %refine_factor)
@@ -33,22 +33,57 @@ def peridym_set_horizon(mesh, horizon=None):
     else:
         return mesh, horizon
 
+def compute_distance(coord1, coord2):
+    """
+    computes the (abs value) of distance
+    between the two coordinates 'coord1' and 'coord2'
+    (for 2D only)
+    input:
+    ------
+        coord1, coord2 : list of floats
+            two coordinates in a 2D space
+    returns:
+    --------
+        distance float:
+            distance between coord1 and coord2
 
 
-#def peridym_compute_neighbors(mesh, horizon):
-#    """TODO: Docstring for peridym_compute_neighbors.
-#    :returns: neighbor_list 
-#    """
-#    neighbor_list = []
-#    elems_centroid = get_elem_centroid(mesh)
-#    for cellX, cellY, cellID in elems_centroid:
-#
-#        temp = elems_centroid
-#        temp.remove(elems_centroid[cellID])
-#
-#        for nextCellX, nextCellY, nextCellID in temp:
-#            dist = math.sqrt((cellX - nextCellX)**2 + (cellY - nextCellY)**2)
-#            if dist < horizon:
-#                neighbor_list.append([nextCellX, nextCellY, nextCellID])
-#
-#    return neighbor_list 
+    """
+    return  math.sqrt((coord1[0] - coord2[0])**2 + (coord1[1] - coord2[1])**2)
+
+
+
+def peridym_compute_neighbors(mesh, horizon):
+    """TODO: Docstring for peridym_compute_neighbors.
+
+    input
+    -----
+    mesh : meshpy.triangle mesh
+    horizon : folat , length of peridynamic horizon
+
+    returns
+    -------
+        neighbor_list: np.array
+            list of peridynamic neighbors for the 
+            given peridynamic horizon
+    """
+    print("computing the neighbor list of the mesh for horizon size of %f" %horizon)
+    neighbor_list = []
+
+    elems = np.array(mesh.elements)
+    points = np.array(mesh.points)
+    elem_centroid = get_elem_centroid(mesh)
+
+    for i in range(len(elems)):
+        temp = elem_centroid.copy()
+        temp.remove(elem_centroid[i])
+        curr_dist = 0.0
+        curr_neighbor_list = []
+        for j in range(len(temp)):
+            curr_dist = compute_distance(elem_centroid[i][0:2], temp[j][0:2])
+            if curr_dist <= horizon : 
+                curr_neighbor_list.append(temp[j][2]) # appending the element ID to neighbor_list
+
+        neighbor_list.append(curr_neighbor_list)
+
+    return neighbor_list

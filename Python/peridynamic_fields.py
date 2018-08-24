cb1ad0531fdc716a4e6f16311f7befb144c4561c
gajendra <gajendra.gulgulia@tum.de>
2018-08-24 15:41:27 +0200

    Working peridym_compute_global_force_density
    initial working function for force density computation
    minor modifications in peridym_compute_extension

--- Python/peridynamic_fields.py
+++ Python/peridynamic_fields.py
@@ -70,7 +70,7 @@ def peridym_initialize(mesh, horizon):
 
 def peridym_compute_extension(nbr_lst, nbr_bnd_vct_lst,
                                 nbr_bnd_ln_lst, nbr_infl_fld_lst, 
-                                mw, disp_vect, elem_area):
+                                mw, disp_vct, elem_area):
     """
     routine for calculation of internal force density for a linear
     peridynamic solid material with gaussian influence function
@@ -84,7 +84,7 @@ def peridym_compute_extension(nbr_lst, nbr_bnd_vct_lst,
                           in the neighborhood list
         :nbr_bnd_ln_lst: length of all bonds in the nbr_lst 
         :inf_fld_lst: list of values of influence field for all nodes in discretization
-        :disp_vect: list of the positions of displaced nodes in the mesh
+        :disp_vct: list of the positions of displaced nodes in the mesh
     
     returns: 
     --------
@@ -98,9 +98,9 @@ def peridym_compute_extension(nbr_lst, nbr_bnd_vct_lst,
     """
 
     #no of nodes
-    length = len(disp_vect)
+    length = len(disp_vct)
     force_vect = np.zeros(length, dtype=float) #internal force
-    theta_vect = np.zeros(length, dtype=float) #dilatation
+    theta_vct = np.zeros(length, dtype=float) #dilatation
     e = [] #extension scalar state
     rel_disp_lst = [] # eta in the pseudo code
     for i in range(length):
@@ -109,39 +109,87 @@ def peridym_compute_extension(nbr_lst, nbr_bnd_vct_lst,
         curr_node_rel_disp_lst  = []
         e_local = []
         for j, idx in enumerate(curr_node_nbr_lst):
-            rel_disp_vect = vect_diff(disp_vect[idx], disp_vect[i])
-            curr_node_rel_disp_lst.append(rel_disp_vect.tolist())
+            rel_disp_vct = vect_diff(disp_vct[idx], disp_vct[i])
+            curr_node_rel_disp_lst.append(rel_disp_vct.tolist())
 
             curr_bnd_vct_coord = nbr_bnd_vct_lst[i][j]
 
-            bond_vect_plus_rel_disp = mod(vect_sum(curr_bnd_vct_coord, rel_disp_vect)) #define mod function in helper
+            bond_vect_plus_rel_disp = mod(vect_sum(curr_bnd_vct_coord, rel_disp_vct)) #define mod function in helper
             ee = bond_vect_plus_rel_disp - mod(curr_bnd_vct_coord) 
             e_local.append(ee)
 
-            theta_vect[i] += 3*nbr_infl_fld_lst[i][j]*mod(curr_bnd_vct_coord)*ee*elem_area[idx] #define mod function in helper
+            theta_vct[i] += 3*nbr_infl_fld_lst[i][j]*mod(curr_bnd_vct_coord)*ee*elem_area[idx] #define mod function in helper
 
 
         rel_disp_lst.append(curr_node_rel_disp_lst)
         e.append(e_local)
 
-    return rel_disp_lst, e, theta_vect
+    return rel_disp_lst, e, theta_vct
+
 
 def peridym_compute_globl_force_density(nbr_lst, nbr_bnd_vct_lst, nbr_bnd_len_lst, 
-                                        nbr_infl_fld_lst, mw, nbr_rel_disp_lst, mw,
-                                        nbr_ext_scalar_st_lst, theta_vect, elem_area ):
-    """TODO: Docstring for peridym_compute_globl_force_density.
-
-    :nbr_lst: TODO
-    :nbr_bnd_vct_lst: TODO
-    :nbr_bnd_len_lst: TODO
-    :nbr_infl_fld_lst: TODO
-    :mw: TODO
-    :nbr_rel_disp_lst: TODO
-    :mw: TODO
-    :nbr_ext_scalar_st_lst: TODO
-    :theta_vect: TODO
-    :elem_area: TODO
-    :returns: TODO
-
+                                        nbr_infl_fld_lst, mw, nbr_rel_disp_lst,
+                                        nbr_ext_scalar_st_lst, theta_vct, disp_vct,
+                                        elem_area ):
+    k = 25.5 #shear modulus of aluminum (google)
+    mu = 6.8e10 #bulk modulus of aluminum (google)
     """
-    pass
+    TODO: Docstring for peridym_compute_globl_force_density.
+
+    input:
+    -----
+        nbr_lst: TODO
+        nbr_bnd_vct_lst: TODO
+        nbr_bnd_len_lst: TODO
+        nbr_infl_fld_lst: TODO
+        mw: TODO
+        nbr_rel_disp_lst: TODO
+        mw: TODO
+        nbr_ext_scalar_st_lst: TODO
+        theta_vct: TODO
+        elem_area: TODO
+        disp_vct: TODO
+    
+    returns: TODO
+    --------
+        force_density_vct : np.array of floats
+        nbr_force_scalar_st_lst : peridym list of doubles 
+        nbr_deformed_bnd_unit_vct_lst : peridym list of doubles
+        ed : deviatoric extension scalar state, peridym list 
+        
+    """
+    force_density_vct = np.zeros((len(disp_vct),2),dtype=float)
+    nbr_force_scalar_st_lst = []
+    nbr_deformed_bnd_unit_vct_lst = []
+    ed = [] #deviatoric extension scalar state
+
+    for i in range(len(disp_vct)):
+        curr_node_nbr_lst = nbr_lst[i]
+        curr_force_scalar_st_lst = []
+        curr_deformed_bnd_unit_vct_lst = []
+        curr_ed = []
+        
+        curr_ext = nbr_ext_scalar_st_lst[i]
+        curr_bnd_len_lst = nbr_bnd_len_lst[i]
+        curr_node_infl_fld_lst = nbr_infl_fld_lst[i]
+        curr_bnd_vct_lst = nbr_bnd_vct_lst[i]
+        curr_node_rel_disp_lst = nbr_rel_disp_lst[i]
+
+        for j, idx in enumerate(curr_node_nbr_lst):
+            eed = curr_ext[j] - theta_vct[i]*curr_bnd_len_lst[j]/3
+            t = (3*k*theta_vct[i]*curr_node_infl_fld_lst[j] + 15*mu*eed)*curr_bnd_len_lst[j]/mw[i]
+            deformed_bnd_vct = np.array(vect_sum(curr_bnd_vct_lst[j], curr_node_rel_disp_lst[j]))
+            deformed_bnd_unit_vct = deformed_bnd_vct/mod(deformed_bnd_vct)
+
+            force_density_vct[i] += t*deformed_bnd_unit_vct*elem_area[idx]
+            force_density_vct[j] -= t*deformed_bnd_unit_vct*elem_area[i]
+
+            curr_deformed_bnd_unit_vct_lst.append(deformed_bnd_unit_vct)
+            curr_ed.append(eed)
+            curr_force_scalar_st_lst.append(t)
+
+        nbr_force_scalar_st_lst.append(curr_force_scalar_st_lst)
+        nbr_deformed_bnd_unit_vct_lst.append(curr_deformed_bnd_unit_vct_lst)
+        ed.append(curr_ed)
+
+    return nbr_force_scalar_st_lst, nbr_deformed_bnd_unit_vct_lst, ed, force_density_vct

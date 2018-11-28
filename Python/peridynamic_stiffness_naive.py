459d1c79820ebced504e0b68581883279cfde64d
gajendra <gajendra.gulgulia@tum.de>
2018-11-28 16:39:42 +0100

    vectorized (again) the stiffness matrix generation script
    This was done once on 20th november when the stiffness matrix naive code
    was working for simple 2D plate without any discontinuities
    
    But since the branch forked out from unvectorized version and
    corrections on boundary conditon application were done, I copy pasted
    the vectorized code (that was already working)

--- Python/peridynamic_stiffness_naive.py
+++ Python/peridynamic_stiffness_naive.py
@@ -4,51 +4,83 @@ import matplotlib.pyplot as plt
 
 from peridynamic_neighbor_data import*
 from peridynamic_boundary_conditions import *
-from mesh_tools import *
+from fenics_mesh_tools import *
 
 def omega_fun1(xi, horizon):
 
-    return -np.exp(-(la.norm(xi,2)**2/(0.5*horizon)**2))*float(la.norm(xi,2)<horizon)
+    if(len(xi) >2): #if we have an array of bond length
+        bnd_len = la.norm(xi, 2, axis=1)
+        return -np.exp(-(bnd_len**2/(0.5*horizon)**2))*(bnd_len<horizon).astype(float)
+    else: # if there's a single bond
+        bnd_len = la.norm(xi, 2, axis=0)
+        return -np.exp(-(bnd_len**2/(0.5*horizon)**2))*float(la.norm(xi,2, axis=0)<horizon)
 
 
-def computeInternalForce(d,u,horizon, nbr_lst, elem_area, mw, bulk, mu, elem_cent):
 
-    num_els=len(elem_area)
-    dim = np.shape(elem_cent[0])[0]
+def computeTheta(u, horizon, nbr_lst, trv_lst,cell_vol, cell_cent, mw, gamma):
+    """computes the dilatation vector, theta
+
+    :u: TODO
+    :horizon: TODO
+    :nbr_lst: TODO
+    :trv_lst: TODO
+    :cell_vol: TODO
+    :mwi: TODO
+    :returns: TODO
+
+    """
+    num_els = len(cell_vol)
+    dim = np.shape(cell_cent[0])[0]
+    theta = np.zeros(num_els, dtype=float)
+
+    for i in trv_lst:
+        curr_nbr = nbr_lst[i]
+        xi = cell_cent[curr_nbr] - cell_cent[i]
+        bnd_len = la.norm(xi, 2, axis=1)
+        eta = u[curr_nbr] - u[i]
+        exten = la.norm((xi+eta), 2, axis=1) - bnd_len
+        omega = omega_fun1(xi, horizon)
+
+        cncell_vol = cell_vol[curr_nbr] #cn stands for curr_nbr
+        theta[i] = sum(3*omega*bnd_len*exten*cncell_vol/mw[i])
+
+    return theta
+
+
+#vectorized version of Felix's code
+def computeInternalForce(d,u,horizon, nbr_lst, cell_vol, cell_cent, mw, bulk, mu, gamma):
+
+    num_els=len(cell_vol)
+    dim = np.shape(cell_cent[0])[0]
    
     #compute dilatation for each node
     theta=np.zeros(num_els, dtype=float)
-    gamma= 4*mu/(3*bulk + 4*mu)
     trv_lst = np.insert(nbr_lst[d],0,d)
-    
-    for i in trv_lst:
-        for j in nbr_lst[i]:
-            xi=elem_cent[j] - elem_cent[i]
-            eta = u[j] - u[i]
-            exten = la.norm((xi + eta),2) - la.norm(xi,2)
-            omega = omega_fun1(xi, horizon)
-            theta[i] += 3*omega*la.norm(xi,2)*exten*elem_area[j]/mw[i]
+
+    theta = computeTheta(u, horizon, nbr_lst, trv_lst, cell_vol, cell_cent, mw, gamma)
     
     # Compute pairwise contributions to the global force density vector
     f=np.zeros((num_els,dim), dtype=float)
     for i in trv_lst:
-        for j in nbr_lst[i]:
-            xi = elem_cent[j] - elem_cent[i]
-            eta = u[j] - u[i]
-
-            omega = omega_fun1(xi,horizon)
-            exten = la.norm((xi + eta),2) - la.norm(xi,2)
-            exten_d = exten - theta[i]*la.norm(xi,2)*gamma/3
-            t = (gamma*bulk*theta[i]*la.norm(xi,2) + 8*mu*exten_d)*omega / mw[i]
-            
-            M = (xi + eta)/la.norm((xi + eta),2)
-            f[i] += t*M*elem_area[j]
-            f[j] -= t*M*elem_area[i]
+        curr_nbr = nbr_lst[i]
+        xi = cell_cent[curr_nbr] - cell_cent[i]
+        bnd_len = la.norm(xi, 2, axis=1)
+        eta = u[curr_nbr] - u[i]
+        xi_plus_eta = xi + eta
+        mod_xi_plus_eta = la.norm(xi_plus_eta, 2, axis=1)
+        exten = mod_xi_plus_eta - bnd_len
+        omega = omega_fun1(xi, horizon)
+        exten_d = exten - theta[i]*bnd_len*gamma/3
+        t = (gamma*bulk*theta[i]*bnd_len + 8*mu*exten_d)*omega/mw[i]
+        M = xi_plus_eta/mod_xi_plus_eta[:,None]
+        
+        f[i] += sum(M*cell_vol[curr_nbr][:,None]*t[:,None])*cell_vol[i]
+        f[curr_nbr] -= M*t[:,None]*cell_vol[curr_nbr][:,None]*cell_vol[i]
 
     return f
+
     
-    
-def computeK(horizon, elem_area, nbr_lst, mw, elem_cent):
+def computeK(horizon, cell_vol, nbr_lst, mw, cell_cent):
 
     print("Beginning to compute the tangent stiffness matrix")
 
@@ -64,11 +96,12 @@ def computeK(horizon, elem_area, nbr_lst, mw, elem_cent):
     gamma= 4*mu / (3*bulk + 4*mu)
 
     # Compose stiffness matrix
-    num_els= len(elem_area)
-    dim = np.shape(elem_cent[0])[0]
-    size = num_els*dim
-    K_naive = np.zeros((size, size), dtype=float)
+    num_els= len(cell_vol)
+    dim = np.shape(cell_cent[0])[0]
+    dof = num_els*dim
+    K_naive = np.zeros((dof, dof), dtype=float)
     small_val=1e-10 #purtub factor
+    inv_small_val = 1.0/small_val
     
     for i in range(num_els):
         for d in range(dim):
@@ -77,71 +110,15 @@ def computeK(horizon, elem_area, nbr_lst, mw, elem_cent):
             u_e_p[i][d]= 1.0*small_val
             u_e_m[i][d]= -1.0*small_val
     
-            f_p=computeInternalForce(i,u_e_p,horizon,nbr_lst,elem_area,mw, bulk,mu,elem_cent)
-            f_m=computeInternalForce(i,u_e_m,horizon,nbr_lst,elem_area,mw, bulk,mu,elem_cent)
+            f_p=computeInternalForce(i,u_e_p,horizon,nbr_lst,cell_vol,cell_cent,mw,bulk,mu,gamma)
+            f_m=computeInternalForce(i,u_e_m,horizon,nbr_lst,cell_vol,cell_cent,mw,bulk,mu,gamma)
             
-            K_naive[0::dim][:,dim*i+d] = (f_p[:,0] - f_m[:,0])/(2*small_val)
-            K_naive[1::dim][:,dim*i+d] = (f_p[:,1] - f_m[:,1])/(2*small_val)
+            for dd in range(dim):
+                K_naive[dd::dim][:,dim*i+d] = (f_p[:,dd] - f_m[:,dd])*0.5*inv_small_val
+            #K_naive[0::dim][:,dim*i+d] = (f_p[:,0] - f_m[:,0])/(2*small_val)
+            #K_naive[1::dim][:,dim*i+d] = (f_p[:,1] - f_m[:,1])/(2*small_val)
     
     end = tm.default_timer()
     print("Time taken for the composition of tangent stiffness matrix seconds: %4.3f\n" %(end-start))
 
     return K_naive
-    
-
-# specify elasticity constants nu and E
-
-#mesh and geometry info
-#m = rectangle_mesh(subdiv=(4,2))
-#for i in range(2):
-#    m = uniform_refine_triangles(m,2);
-#
-#horizon = 1.5*max(get_edge_lengths(m));
-#
-#elem_area = get_elem_areas(m)
-#elem_cent = get_elem_centroid(m)
-#dim = np.shape(elem_cent[0])[0]
-#num_els = np.shape(elem_cent)[0]
-#nbr_lst, _, _, _, mw = peridym_get_neighbor_data(m, horizon)
-
-
-# Compute weighted volume for each node
-#mw=np.zeros(num_els, dtype=float)
-#for i in range(num_els):
-#    for j in nbr_lst[i]:
-#        xi=elem_cent[j] - elem_cent[i]
-#        omega = omega_fun1(xi, horizon)
-#        mw[i] += omega*la.norm(xi,2)**2*elem_area[j]
-#    
-
-# Compute tangent stiffness matrix
-#K = computeK(horizon, elem_area, nbr_lst, mw, bulk , mu, elem_cent)
-
-#print("applying boundary conditions")
-#
-##boundary conditions given out as key value pair
-#bc = {'left':'dirichlet', 'right':'force'}
-#K_bc, fb = peridym_apply_bc(m, K, bc, -2.5e10);
-#
-#print("solving the linear system")
-#sol = la.solve(K_bc,fb)
-#
-#
-##to plot the displacement do some work
-#u_disp = copy.deepcopy(sol)
-#u_disp = np.reshape(u_disp, (int(len(sol)/dim), dim))
-#
-#a, b = get_peridym_mesh_bounds(m)
-#node_ids = a['left']
-#ll = b['left']
-#
-#for i, nk in enumerate(node_ids):
-#    u_disp = np.insert(u_disp, nk, np.zeros(dim, dtype=float), axis=0)
-#
-#x, y = elem_cent.T
-#disp_cent = elem_cent + u_disp
-##plot_(m, new_fig=True, annotate=False)
-#plt.scatter(x,y, color='r', marker='o')
-#x,y = disp_cent.T
-#plt.scatter(x,y, color='b', marker='o')
-#plt.show(block=False)

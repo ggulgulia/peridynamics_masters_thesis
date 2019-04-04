db9c73c32aef88d619ef8258d8a8852faa15bcb3
gajendra <gajendra.gulgulia@tum.de>
2019-04-04 16:35:08 +0200

    initialize a remote repo in gitlab for VM2

--- Python/validations/testScriptInflFuns.py
+++ Python/validations/testScriptInflFuns.py
@@ -28,7 +28,7 @@ def compare_PD_infl_funs_with_FE(mesh_lst, u_fe_conv, fig_cnt, data_path=None, m
     """
     #infl_fun_lst = {'omega1':gaussian_infl_fun2, 'omega2':gaussian_infl_fun1, 'omega3': parabolic_infl_fun2, 'omega4':parabolic_infl_fun1, }
 
-    infl_fun_lst = {'omega2':gaussian_infl_fun1}
+    infl_fun_lst = {'omega2':gaussian_infl_fun1, 'omega1':gaussian_infl_fun2}
 
     infl_fun_keys = infl_fun_lst.keys()
     infl_fun_name = {'omega1':'STANDARD GAUSSIAN', 'omega2': 'NARROW GAUSSIAN', 'omega3': 'STANDARD PARABOLA', 'omega4': 'PERIDIGM PARABOLA'}
